26562195d36100790c41939d038c58e947189428
engali1030 <engali1030@gmail.com>
2024-04-24 22:56:31 +0200

    commit-2

---
+++ @types/js/article.js
@@ -0,0 +1,33 @@
+
+$(".son-link div").on("click", function (e) {
+  $(".son-link div").removeClass("active");
+  $(this).addClass("active");
+  if (e.target.classList.contains("one")) {
+    $(".maragaa").addClass("d-none");
+    $(".taws.maragaa").removeClass("d-none");
+    $(".taws.maragaa").addClass("d-block");
+  } else {
+    $(".maragaa").removeClass("d-none");
+    $(".maragaa").addClass("d-block");
+    $(".taws.maragaa").addClass("d-none");
+  }
+});
+
+
+/**********video************* */
+ const playerr = new Plyr("#playerr", {
+   controls: [
+     "play-large",
+     "progress",
+     "current-time",
+     "captions",
+     "fullscreen",
+   ],
+   youtube: {
+     noCookie: true,
+     rel: 0,
+     showinfo: 0,
+     iv_load_policy: 3,
+   },
+ });
+ 
